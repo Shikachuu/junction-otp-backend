35750e4a06d11bb2c10a8e8a9d6081da7a56c7ce
Battlechicken <battlechicken74@gmail.com>
2019-10-26 13:41:15 +0200

    Added demo project for google maps api

---
+++ GoogleMapsApiTest/GoogleMapsApiTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GoogleMapsApiTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            GoogleApiFetcher googleApiFetcher = new GoogleApiFetcher();
+            var result = googleApiFetcher.DemoFetch();
+
+        }
+    }
+}
