a11f0cf77adf55e132c4463f27a3512ae5f18d31
Battlechicken <battlechicken74@gmail.com>
2019-10-26 17:34:20 +0200

    haladás

---
+++ Backend/RouteWithAtm.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace junctionx_backend
+{
+    public class Atm
+    {
+        
+        public string AtmPosition { get; set; }
+
+        public string StreetName { get; set; }
+
+        public string UserFriendlyRoute { get; set; }
+
+    }
+}
