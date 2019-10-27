330ce9d9b27b97276bac3b91269e6e18665ffe2e
Battlechicken <battlechicken74@gmail.com>
2019-10-27 02:53:37 +0100

    android dolgok

--- Frontend/app/src/main/java/junctionx/hackathon/otp/MapsActivity.kt
+++ Frontend/app/src/main/java/junctionx/hackathon/otp/MapsActivity.kt
@@ -10,6 +10,11 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
+import org.json.JSONObject
+import org.json.JSONTokener
+import java.net.URL
+
+
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
@@ -32,6 +37,22 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+
+    fun FetchDataForDestination(destination: String) {
+        // http://localhost:5000/atm?origin=47.475828,19.099312&destination=47.510687,19.055810
+        var ip = "100.98.11.34"
+        var url = "http://$ip:5000/atm?origin=$destination&destination=47.510687,19.055810"
+
+
+        var jsonAsString = URL(url).readText()
+
+        var rootObject = JSONObject(jsonAsString)
+
+        rootObject.
+
+    }
+
+
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
@@ -39,5 +60,17 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+        mMap.setOnMapClickListener {
+            var postitionAsString = it.latitude.toString() + "," + it.longitude.toString();
+            Thread {
+                var result = FetchDataForDestination(postitionAsString)
+
+            }
+
+        }
+//        google.maps.event.addListener(map, 'click', function(event) {
+//            placeMarker(event.latLng);
+//        });
     }
 }
